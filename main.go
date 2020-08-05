f3cee5c608065bca70891223da22215bd6bff388
Steveen <steveen.echeverri@gmail.com>
2020-08-05 18:41:52 -0500

    Listening for requests http.ListenAndServe method

--- main.go
+++ main.go
@@ -2,13 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func main()  {
 	http.HandleFunc("/welcome", welcome)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	} 	
 }
 
+// func ListenAndServe(addr string, handler Handler) error
+
 func welcome(w http.ResponseWriter, r *http.Request)  {
-	fmt.Fprintln(w, "Welcome to th GO server..")
+	fmt.Fprintln(w, "Welcome to the GO server..")
 }
